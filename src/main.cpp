8743acce28eabb6b9543d89d869de57a25fa1d95
LimeGradient <87611796+LimeGradient@users.noreply.github.com>
2024-09-20 11:04:34 -0400

    mannnnnn

--- src/main.cpp
+++ src/main.cpp
@@ -32,7 +32,7 @@ class $modify(SharingEndLevelLayer, EndLevelLayer) {
 	void onTweetButton(CCObject*) {
 		geode::createQuickPopup("Look I Did It", "Would you like to share this completion on Twitter?", "No", "Yes", [=](auto, bool btn2) {
 			if (btn2) {
-				auto levelName = this->m_playLayer->m_level->m_levelName;
+				std::string levelName = this->m_playLayer->m_level->m_levelName;
 				auto creatorName = this->m_playLayer->m_level->m_creatorName;
 				auto attempts = this->m_playLayer->m_level->m_attempts.value();
 				auto levelID = this->m_playLayer->m_level->m_levelID.value();
