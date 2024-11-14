98c19d27ae2f34d166aa0ee8c94453312073e59c
Erymanthus | RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>
2024-11-13 20:03:59 -0500

    one last 3.90 commit

--- src/main.cpp
+++ src/main.cpp
@@ -314,5 +314,6 @@ class $modify(SharingEndLevelLayer, EndLevelLayer) {
 		addDiscord(menu);
 		addWeb(menu);
 		addScreenshot(menu);
+		if (menu->getChildrenCount() < 1) menu->removeMeAndCleanUp();
 	}
 };
